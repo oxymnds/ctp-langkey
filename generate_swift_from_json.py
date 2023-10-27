350947c91344b69ba388cd3a59392348a899aa96
0xymnds <ton@gritthailand.com>
2023-10-27 22:04:39 +0700

    feat: update swift gen

---
+++ generate_swift_from_json.py
@@ -0,0 +1,69 @@
+import os
+import json
+import sys
+
+def camel_case_conversion(s):
+    """Converts a string to CamelCase."""
+    return ''.join(word.capitalize() for word in s.split('_'))
+
+def gather_keys(data, path=''):
+    """Recursively gather all the keys and their paths from the JSON data."""
+    keys = []
+    for k, v in data.items():
+        new_key = f"{path}.{k}" if path else k
+        if isinstance(v, dict):
+            keys.extend(gather_keys(v, new_key))
+        else:
+            keys.append(new_key)
+    return keys
+
+def generate_swift_enum(keys_structure):
+    """Generate Swift enum code based on the JSON keys structure."""
+    grouped_keys = {}
+    for key in keys_structure:
+        top_level_key = key.split('.')[0]
+        camel_case_key = camel_case_conversion(top_level_key)
+        if camel_case_key not in grouped_keys:
+            grouped_keys[camel_case_key] = []
+        grouped_keys[camel_case_key].append(key)
+
+    swift_files = {}
+    for camel_case_key, keys in grouped_keys.items():
+        swift_code = f"public extension LangKey {{\n    enum {camel_case_key}: String {{\n"
+        for key in keys:
+            components = key.split('.')
+            if len(components) >= 3:
+                case_name = camel_case_conversion(f"{components[1]}_{components[2]}").lower()
+            else:
+                case_name = camel_case_conversion(components[-1]).lower()
+            swift_code += f"        case {case_name} = \"{key}\"\n"
+        swift_code += "    }\n}"
+        swift_files[f"LangKey+{camel_case_key}.swift"] = swift_code
+
+    return swift_files
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Usage: python script_name.py path_to_json_file")
+        sys.exit(1)
+    
+    json_file_path = sys.argv[1]
+    
+    with open(json_file_path, 'r', encoding='utf-8') as f:
+        data = json.load(f)
+    
+    keys_structure = gather_keys(data)
+    swift_files = generate_swift_enum(keys_structure)
+
+    # Ensure the "output" directory exists relative to the current directory
+    output_dir = "output"
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    for file_name, content in swift_files.items():
+        # Prepend the directory path to the filename
+        output_path = os.path.join(output_dir, file_name)
+        with open(output_path, 'w', encoding='utf-8') as f:
+            f.write(content)
+    
+    print(f"Generated {len(swift_files)} Swift files in {output_dir}.")
